ea992478f335bb43fc6fb698ddc98cff093b1596
Freshta Ebrahim <freshtaebrahim@hotmail.co.uk>
2020-12-13 00:46:20 +0000

    link added to end block

--- src/components/CVEndBlock/index.js
+++ src/components/CVEndBlock/index.js
@@ -4,7 +4,10 @@ import "./CVEndBlock.css";
 export default function CVEndBlock() {
   return (
     <div className="CVEndBlock">
-      <div className="CVEndBlock-text">This CV is created in React</div>
+      <div className="CVEndBlock-text">
+        This CV is created in React, available to view here:
+        <a href="https://freshtaebrahim.github.io/">freshtaebrahim.github.io</a>
+      </div>
     </div>
   );
 }
